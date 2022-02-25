f08a3b128ee822612e7b6832803880383c80401e
martinsph <ph.mmartins@gmail.com>
2022-02-25 16:39:01 -0300

    Fix: fix lint

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -1,10 +1,6 @@
 from collections.abc import Iterable
-import os
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
-from inventory_report.importer.json_importer import JsonImporter
-from inventory_report.importer.csv_importer import CsvImporter
-from inventory_report.importer.xml_importer import XmlImporter
 from inventory_report.inventory.inventory_iterator import InventoryIterator
 
 

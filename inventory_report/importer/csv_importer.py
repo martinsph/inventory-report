4fe289be1e89561daad67a996763f7d94a34f315
martinsph <ph.mmartins@gmail.com>
2022-02-25 14:35:46 -0300

    feat: add req 3, 4, 5 e 6

---
+++ inventory_report/importer/csv_importer.py
@@ -0,0 +1,17 @@
+import csv
+from os.path import splitext
+from inventory_report.importer.importer import Importer
+
+
+class CsvImporter(Importer):
+    def import_data(path):
+        _, extension = splitext(path)
+
+        if (extension != '.csv'):
+            raise ValueError('Arquivo inválido')
+
+        with open(path) as file:
+            result = csv.DictReader(file, delimiter=",", quotechar='"')
+            products = list(result)
+
+        return products
---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,22 @@
+import os
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.xml_importer import XmlImporter
+
+
+class Inventory():
+    def import_data(path, report_type):
+        file_name, extension = os.path.splitext(path)
+        if extension == ".csv":
+            raw_file = CsvImporter.import_data(path)
+        elif extension == ".json":
+            raw_file = JsonImporter.import_data(path)
+        elif extension == ".xml":
+            raw_file = XmlImporter.import_data(path)
+
+        if report_type == "simples":
+            return SimpleReport.generate(raw_file)
+
+        return CompleteReport.generate(raw_file)
